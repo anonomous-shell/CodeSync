11ecce9de7231d036be2757b972bf2894b39b590
CodeSync <codesync@users.noreply.github.com>
2026-08-17 00:15:40 +0530

    Solved: SQL CREATE TABLE (GeeksforGeeks) Sheets: GFG160

--- CodeSync/GFG 160/SQL CREATE TABLE/solution.py
+++ CodeSync/GFG 160/SQL CREATE TABLE/solution.py
@@ -6,7 +6,7 @@
 # Topics: Placement 360 Course Explore, Placement 360 Course, Explore
 # Runtime: N/A
 # Memory: N/A
-# Synced: 2026-08-16T18:44:37.161Z
+# Synced: 2026-08-16T18:45:37.167Z
 
 CREATE TABLE table_name
 (
