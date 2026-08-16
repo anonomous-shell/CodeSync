d4e01333eb9339094b53506530f092c3267b9780
CodeSync <codesync@users.noreply.github.com>
2026-08-17 00:11:03 +0530

    Solved: SQL CREATE TABLE (GeeksforGeeks) Sheets: GFG160

---
+++ CodeSync/GeeksforGeeks/all/SQL CREATE TABLE/solution.py
@@ -0,0 +1,18 @@
+# Platform: GeeksforGeeks
+# Problem: SQL CREATE TABLE
+# URL: https://www.geeksforgeeks.org/sql/sql-create-table/
+# Language: Python
+# Difficulty: Unknown
+# Topics: Placement 360 Course Explore, Placement 360 Course, Explore
+# Runtime: N/A
+# Memory: N/A
+# Synced: 2026-08-16T18:40:41.606Z
+
+CREATE TABLE table_name
+(
+Column1 datatype (size),
+column2 datatype (size),
+.
+.
+columnN datatype(size)
+);
